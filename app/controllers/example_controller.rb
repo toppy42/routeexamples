25a645c981c6829d63db840ddf7e08d4e35b2e0f
Ethan Puzarne <epuzarne@wespire.com>
2017-06-25 22:04:29 -0400

    remove route/controller exercises

--- app/controllers/example_controller.rb
+++ app/controllers/example_controller.rb
@@ -1,14 +1,6 @@
 class ExampleController < ApplicationController
   def example1
-    render plain:  2+2
-  end
-
-  def example2
-    render plain: params[:number]
-  end
-
-  def example3
-    render plain: params[:number].to_i + params[:number2].to_i
+    render plain: "Example 1"
   end
 
   def example_put
@@ -17,9 +9,5 @@ class ExampleController < ApplicationController
 
   def example_post
     render plain: "You did a post"
-  end
-
-  def proof_you_can_name_this_whatever_123_abc
-    render plain: "You should not give methods random names. But you can"
   end
 end
